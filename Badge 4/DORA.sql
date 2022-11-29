c4abba0e291a40067d0bf87f873857304120ca31
Rebecca <54799857+rebeccamoen@users.noreply.github.com>
2022-11-29 12:54:43 +0100

    Create DORA.sql

---
+++ Badge 4/DORA.sql
@@ -0,0 +1,94 @@
+-- DORA
+use role accountadmin;
+
+select demo_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from
+(SELECT 
+ 'DORA_IS_WORKING' as step
+ ,(select 123 ) as actual
+ ,123 as expected
+ ,'Dora is working!' as description
+); 
+
+-- DORA 1
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+ 'DLKW01' as step
+  ,(
+      select count(*)  
+      from ZENAS_ATHLEISURE_DB.INFORMATION_SCHEMA.STAGES 
+      where stage_url ilike ('%/clothing%')
+      or stage_url ilike ('%/zenas_metadata%')
+      or stage_url like ('%/sneakers%')
+   ) as actual
+, 3 as expected
+, 'Stages for Klaus bucket look good' as description
+); 
+
+-- DORA 2
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+   'DLKW02' as step
+   ,(select sum(tally) from
+        (select count(*) as tally
+        from ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATBAND_PRODUCT_LINE
+        where length(product_code) > 7 
+        union
+        select count(*) as tally
+        from ZENAS_ATHLEISURE_DB.PRODUCTS.SWEATSUIT_SIZES
+        where LEFT(sizes_available,2) = char(13)||char(10))     
+     ) as actual
+   ,0 as expected
+   ,'Leave data where it lands.' as description
+); 
+
+-- DORA 3
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+ SELECT
+ 'DLKW03' as step
+ ,(select count(*) from ZENAS_ATHLEISURE_DB.PRODUCTS.CATALOG) as actual
+ ,198 as expected
+ ,'Cross-joined view exists' as description
+); 
+
+-- DORA 4
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW04' as step
+ ,(select count(*) 
+  from zenas_athleisure_db.products.catalog_for_website 
+  where upsell_product_desc like '%NUS:%') as actual
+ ,6 as expected
+ ,'Relentlessly resourceful' as description
+); 
+
+-- DORA 5
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW05' as step
+ ,(select sum(tally)
+   from
+     (select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.stages 
+      union all
+      select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.file_formats)) as actual
+ ,4 as expected
+ ,'Camila\'s Trail Data is Ready to Query' as description
+ ); 
+ 
+ -- DORA 6
+select DEMO_DB.PUBLIC.GRADER(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT
+'DLKW06' as step
+ ,(select count(*) as tally
+      from mels_smoothie_challenge_db.information_schema.views 
+      where table_name in ('CHERRY_CREEK_TRAIL','DENVER_AREA_TRAILS')) as actual
+ ,2 as expected
+ ,'Mel\'s views on the geospatial data from Camila' as description
+ ); 
