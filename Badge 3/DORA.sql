3ab7d05387d716030ab640f0312ceb597651735f
Rebecca <54799857+rebeccamoen@users.noreply.github.com>
2022-11-29 12:48:03 +0100

    Update DORA.sql

--- Badge 3/DORA.sql
+++ Badge 3/DORA.sql
@@ -93,3 +93,35 @@ select grader(step, (actual = expected), actual, expected, description) as grade
 , 265 as expected
 ,'Simple Currency Looks Good' as description
 );
+
+-- DORA 8-11
+select grader(step, (actual = expected), actual, expected, description) as graded_results from
+(
+SELECT 
+ 'SMEW08' as step
+ ,(select count(*)/NULLIF(count(*),0) from snowflake.reader_account_usage.query_history
+where USER_NAME = 'MANAGED_READER_ADMIN' and query_text ilike ('%366%')) as actual
+ , 1 as expected
+ ,'03-00-01-08' as description
+UNION ALL
+SELECT 
+  'SMEW09' as step
+ ,(select count(*)/NULLIF(count(*),0) from snowflake.reader_account_usage.query_history
+where USER_NAME = 'MANAGED_READER_ADMIN' and query_text ilike ('%NCIES%')) as actual
+ , 1 as expected
+ ,'03-00-01-09' as description
+UNION ALL
+SELECT 
+  'SMEW10' as step
+ ,(select count(*)/NULLIF(count(*),0) from snowflake.reader_account_usage.query_history
+where USER_NAME = 'MANAGED_READER_ADMIN' and query_text ilike ('%IMPLE%')) as actual
+ , 1 as expected
+ ,'03-00-01-10' as description
+UNION ALL 
+SELECT 
+    'SMEW11' as step 
+,(select count(*)/NULLIF(count(*),0) from snowflake.reader_account_usage.query_history
+where USER_NAME = 'MANAGED_READER_ADMIN' and query_text ilike ('%DE_TO%')) as actual
+, 1 as expected
+,'03-00-01-11' as description
+); 
